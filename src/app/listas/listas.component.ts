55c00cf87489f2e61ade89ba5cd5f8f54778c02f
Lautaro Bidegorry <bidegorrylb1@gmail.com>
2024-03-27 16:45:36 -0300

    e-listas

---
+++ src/app/listas/listas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listas',
+  templateUrl: './listas.component.html',
+  styleUrl: './listas.component.scss'
+})
+export class ListasComponent {
+  lista1 =["Maria","Julieta","Victoria","Cristina"];
+  lista2 =["Julio","Carlos","Pedro","Juan"];
+}
